5604321db2cabab55edddb7635a9d12d9a71d389
josepmarti <josep.marti.elias@gmail.com>
2023-02-04 20:24:35 +0100

    extras to script.js

--- js/scripts.js
+++ js/scripts.js
@@ -29,3 +29,10 @@ $(document).ready(function () {
     });
 
 });
+
+/********************** Extras *********************/
+
+// alert_markup
+function alert_markup(alert_type, msg) {
+    return '<div class="alert alert-' + alert_type + '" role="alert">' + msg + '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span>&times;</span></button></div>';
+}
