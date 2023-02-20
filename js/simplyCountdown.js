e5544be5c816b8f082f330b1ac77d007e1730a71
xavi <xaviervergestarragona@gmail.com>
2023-02-20 22:17:02 +0100

    minorerrors9

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -118,7 +118,7 @@
                 year: 2023,
                 month: 7,
                 day: 8,
-                hours: 17,
+                hours: 19,
                 minutes: 0,
                 seconds: 0,
                 words: {
